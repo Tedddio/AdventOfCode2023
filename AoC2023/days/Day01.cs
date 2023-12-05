1a190817eed7b8e3ecd50af75151e2f7bf28db48
Cedrick Therrien <therrien.cedrick@gmail.com>
2023-12-05 07:45:39 -0500

    feat: resolved day1 puzzle 2

--- AoC2023/days/Day01.cs
+++ AoC2023/days/Day01.cs
@@ -6,11 +6,13 @@ public class Day01
 {
     private readonly string[] input;
     private readonly Stopwatch timer;
+    private readonly IDictionary<int, string> stringifiedDigits;
 
     public Day01()
     {
         timer = new Stopwatch();
         input = File.ReadAllLines("inputs/day01.txt");
+        stringifiedDigits = GetStringifiedDigits();
     }
 
     public void Solve()
@@ -18,6 +20,9 @@ public class Day01
         timer.Start();
         var result1 = SolveProblem1();
         Console.WriteLine($"Day 1 result 1 is {result1}. Completed in {timer.Elapsed}");
+        timer.Restart();
+        var result2 = SolveProblem2();
+        Console.WriteLine($"Day 1 result 2 is {result2}. Completed in {timer.Elapsed}");
     }
 
     private int SolveProblem1()
@@ -30,5 +35,60 @@ public class Day01
             sum += int.Parse($"{firstDigit}{lastDigit}");
         };
         return sum;
+    }
+
+
+    private int SolveProblem2()
+    {
+        var sum = 0;
+        foreach (var s in input)
+        {
+            var firstDigit = FindFirstDigitIncludingStringified(s);
+            var lastDigit = FindFirstDigitIncludingStringified(s, true);
+            sum += int.Parse($"{firstDigit}{lastDigit}");
+        };
+        return sum;
+    }
+
+    private string FindFirstDigitIncludingStringified(string line, bool fromEnd = false) 
+    {
+        var firstDigit = fromEnd ? line.LastOrDefault(char.IsDigit)
+            : line.FirstOrDefault(char.IsDigit);
+
+        var occurences = stringifiedDigits
+            .Select(s => new KeyValuePair<int, string>(line.GetIndex(s.Value, fromEnd), s.Key.ToString()))
+            .Where(result => result.Key != -1)
+            .OrderBy(x => x.Key);
+        
+        var firstStringOccurence =  fromEnd ? occurences.LastOrDefault()
+            : occurences.FirstOrDefault();
+
+        if (firstStringOccurence.Equals(default(KeyValuePair<int, string>)))
+        {
+            return firstDigit.ToString();
+        }
+
+        if ((fromEnd && line.LastIndexOf(firstDigit) > firstStringOccurence.Key )
+            || (!fromEnd && line.IndexOf(firstDigit) < firstStringOccurence.Key))
+        {
+            return firstDigit.ToString();
+        } 
+        return firstStringOccurence.Value;
+    }
+
+    private IDictionary<int, string> GetStringifiedDigits()
+    {
+        return new Dictionary<int, string>()
+        {
+            {1, "one"},
+            {2, "two"},
+            {3, "three"},
+            {4, "four"},
+            {5, "five"},
+            {6, "six"},
+            {7, "seven"},
+            {8, "eight"},
+            {9, "nine"},
+        };
     }
 }
---
+++ AoC2023/helpers/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC2023;
+
+public static class StringExtensions
+{
+    public static int GetIndex(this string line, string value, bool fromEnd = false)
+    {
+        return fromEnd ? line.LastIndexOf(value)
+            : line.IndexOf(value);
+    }
+}
